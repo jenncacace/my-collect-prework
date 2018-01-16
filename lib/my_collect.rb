09c368ed6c4fae6a164d1b40cd195e3411c28a92
Jenn Cacace <jenn.cacace@gmail.com>
2018-01-16 14:20:50 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   new_array = []
   i = 0   
   while i < array.length 
-    return_array << yield array[i]
+    return_array << yield (array[i])
     i += 1 
   end 
   new_array 
